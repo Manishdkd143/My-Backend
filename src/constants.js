fb490d755913e2fa7f7522f2e9bca558ca6c0b35
Manishdkd143 <Manishdhakad060@gmail.com>
2025-09-05 13:51:01 +0530

    add models folder in src

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const DB_NAME="USERDB"
+export const PORT=3000
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import express from "express"
+
+import { DB_NAME,PORT } from "./constants.js";
+
+ (async()=>{
+try {
+    console.log(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+   app.on('error',(error)=>{
+      console.log("Errrr",error)
+      throw error
+   })
+   app.listen(PORT,()=>{
+    console.log(`app is listening on ${PORT}`)
+   })
+} catch (error) {
+    console.log("Connection is not establish",error)
+    throw error
+}
+ })()
