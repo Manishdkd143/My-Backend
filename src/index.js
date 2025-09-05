196c67e6f580351bb5a6d64174423c53c5612fda
Manishdkd143 <Manishdhakad060@gmail.com>
2025-09-05 14:24:20 +0530

    Add db Connection files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 import mongoose from "mongoose";
 import express from "express"
-
+import dotenv from "dotenv"
 import { DB_NAME,PORT } from "./constants.js";
+import ConnectDB from "./db/Connect.js";
 
- (async()=>{
-try {
-    console.log(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-   app.on('error',(error)=>{
-      console.log("Errrr",error)
-      throw error
-   })
-   app.listen(PORT,()=>{
-    console.log(`app is listening on ${PORT}`)
-   })
-} catch (error) {
-    console.log("Connection is not establish",error)
-    throw error
-}
- })()
+
+dotenv.config({
+    path:'./env'
+})
+ConnectDB()
+
+//  (async()=>{
+// try {
+//     console.log(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//    app.on('error',(error)=>{
+//       console.log("Errrr",error)
+//       throw error
+//    })
+//    app.listen(PORT,()=>{
+//     console.log(`app is listening on ${PORT}`)
+//    })
+// } catch (error) {
+//     console.log("Connection is not establish",error)
+//     throw error
+// }
+//  })()
