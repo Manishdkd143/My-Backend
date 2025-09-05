83c736ba74d8e27762bb123485c4b1307d619769
Manishdkd143 <Manishdhakad060@gmail.com>
2025-09-05 15:39:44 +0530

    Utils files attached and errors order sequential

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import express from "express"
 import dotenv from "dotenv"
 import { DB_NAME,PORT } from "./constants.js";
 import ConnectDB from "./db/Connect.js";
-
-
+const app=express()
 dotenv.config({
     path:'./env'
 })
 ConnectDB()
+.then(()=>{
+   app.on('error',(err)=>{
+    console.log("Connection is failed",err)
+   })
+   app.listen(PORT,()=>{
+    console.log(`app is listening on ${PORT}`)
+   })
+})
+.catch((err)=>{
+    console.log("Error",err)
+})
 
 //  (async()=>{
 // try {
