452da25f783b12c560e4366bb24f7febc9ac72af
Manishdkd143 <Manishdhakad060@gmail.com>
2025-09-22 08:54:49 +0530

    modified routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -26,8 +26,8 @@ router.route("/refresh-token").post(refreshAccessToken);
 router.route("/updated-details").patch(verifyJWT,updateUserDetails)
 router.route("/change-password").patch(verifyJWT,updateUserPassword);
 router.route("/current-user").get(verifyJWT,getCurrentUser);
-router.route("/avatar").post(verifyJWT,upload.single("avatar"),updateUserAvatar)
-router.route("/coverImage").post(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/coverImage").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
 router.route("/channel/:username").get(verifyJWT,getUserProfileDetails);
 router.route("/history").get(verifyJWT,getWatchHistory)
 export default router
